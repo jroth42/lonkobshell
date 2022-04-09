4389d6f8574b2b1ed4a2ff352f0fb78681282564
Long Ngo <hoang.long@web.de>
2022-04-09 18:20:01 +0200

    unfinished unset

--- built_in/export.c
+++ built_in/export.c
@@ -29,19 +29,24 @@ int len_til_equal(char *str)
     return (i);
 }
 
-int compare_env(char *env_str, char *str)
+int compare_env(char *env_str, char *str, int mode)
 {
     int len;
     len = 0;
-    if (has_equal(str))
+    if (has_equal(str) && mode == 1)
     {
         len = len_til_equal(str);
         if (ft_strncmp(env_str, str, len) == 0)
             return (1);
     }
+    else if (!has_equal(str) && mode == 0)
+    {
+        len = len_til_equal(env_str);
+        if (ft_strncmp(env_str, str, len) == 0)
+            return (1);
+    }
     return (0);
 }
-
 
 int is_env(t_env *node, char *str)
 {
@@ -50,34 +55,41 @@ int is_env(t_env *node, char *str)
     tmp = node;
     while (tmp)
     {
-        if (compare_env(tmp->str, str))
+        if (compare_env(tmp->str, str, 1))
             return (1);
         tmp = tmp->next;
     }
     return (0);
 }
 
-void    modify_env(t_env **node, char *str)
+// modifies the env_node depending on mode; 1 = export -> modify string   0 = unset -> delete node
+void    modify_env(t_env **node, char *str, int mode)
 {
     t_env *tmp;
 
     tmp = *node;
+    tmp->flag = 0;
     while (tmp)
     {
-        if (compare_env(tmp->str, str))
+        if (compare_env(tmp->str, str, 1) && mode == 1)
         {
 			//free(tmp->str);
             tmp->str = ft_strdup(str);
             return ;
         }
+        else if (compare_env(tmp->str, str, 0) && mode == 0)
+        {
+            tmp->flag = 1;
+            unset_env(node);
+        }
         tmp = tmp->next;
     }
-}
+} 
 
 void    built_export(t_env **node, char *str)
 {
     if (is_env(*node, str))
-        modify_env(node, str);
+        modify_env(node, str, 1);
 /*     else
         env_add_back(node, str);   */
 }
