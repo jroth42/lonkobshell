f8ae23a4b224fc9900e2975fe200dabc66b0ecb3
jroth42 <jakob.roth@live.de>
2022-04-24 23:39:21 +0200

    fixed echo function

--- built_in/echo.c
+++ built_in/echo.c
@@ -6,7 +6,7 @@
 /*   By: jroth <jroth@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 20:05:23 by jroth             #+#    #+#             */
-/*   Updated: 2022/04/24 22:13:21 by jroth            ###   ########.fr       */
+/*   Updated: 2022/04/24 23:36:56 by jroth            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,39 +25,13 @@ int	ft_pwd(void)
 	return (g_exit);
 }
 
-static bool	check_valid_opt(char *opt)
+void	check_echoflag(char *arg, bool *opt, int *i)
 {
-	int	i;
-
-	i = 0;
-	if (ft_strncmp(opt, "-n", 2))
-		return (false);
-	i += 2;
-	while (opt[i])
+	if (!ft_strcmp(arg, "-n"))
 	{
-		if (opt[i] != 'n')
-			return (false);
-		i++;
+		*opt = true;
+		(*i)++;
 	}
-	return (true);
-}
-
-static int	skip_valid_opt(char **args, bool *opt)
-{
-	int	i;
-
-	i = 0;
-	if (!check_valid_opt(args[i]))
-		return (i);
-	*opt = true;
-	i++;
-	while (args[i])
-	{
-		if (!check_valid_opt(args[i]) && ft_strcmp(args[i], ""))
-			break ;
-		i++;
-	}
-	return (i);
 }
 
 void	ft_echo(t_cmd *cmd)
@@ -66,20 +40,20 @@ void	ft_echo(t_cmd *cmd)
 	int		i;
 
 	opt = false;
-	if (get_arr_size(cmd->exec) > 0)
+	if (get_arr_size(cmd->exec) > 1)
 	{
-		i = skip_valid_opt(cmd->exec, &opt);
-        if (i == 0)
-            i = 1;
+		i = 1;
+		check_echoflag(cmd->exec[i], &opt, &i);
 		while (cmd->exec[i])
 		{
-			if (ft_strlen(cmd->exec[i]))
-				printf("%s", cmd->exec[i]);
-			if (ft_strcmp(cmd->exec[i], "")
-				&& cmd->exec[i + 1]
-				&& ft_strcmp(cmd->exec[i + 1], ""))
-				printf(" ");
-			i++;
+			if (cmd->exec[i])
+			{
+				if (cmd->exec[i])
+					printf("%s", cmd->exec[i]);
+				if (!opt)
+					printf(" ");
+				i++;
+			}
 		}
 	}
 	if (!opt)
